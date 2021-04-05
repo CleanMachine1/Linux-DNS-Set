2c1b86c0d65f88295e84e5706ba2704d61c68d82
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-05 11:53:50 +0000

    Full Commit

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install resolvconf -y
+sudo systemctl enable resolvconf.service && sudo systemctl start resolvconf.service
+read -p "Enter DNS IP Primary: " ip1
+sudo echo "nameserver $ip1" > /etc/resolvconf/resolv.conf.d/head
+sudo systemctl restart resolvconf.service
+sudo resolvconf --enable-updates
+sudo resolvconf -u
+
+echo "If you need to add another DNS IP"
+echo "Type in the Input Section"
+echo "OR type 'exit' if you don't"
+read -p "Input: " ans
+if [ $ans = "exit" ]; then
+    exit
+else 
+    sudo echo "nameserver $ans" >> /etc/resolvconf/resolv.conf.d/head
+    sudo systemctl restart resolvconf.service
+    sudo resolvconf --enable-updates
+    sudo resolvconf -u
+
+fi
