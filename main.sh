6b352dee8c04b71d5fc2692bfdf8bb1e2879bc76
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-05 13:15:29 +0100

    Remove clutter outputs

--- main.sh
+++ main.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install resolvconf -y
-sudo systemctl enable resolvconf.service && sudo systemctl start resolvconf.service
+sudo apt update > /dev/null && sudo apt install resolvconf -y > /dev/null
+sudo systemctl enable resolvconf.service > /dev/null && sudo systemctl start resolvconf.service > /dev/null
 read -p "Enter DNS IP Primary: " ip1
 echo "nameserver $ip1" > /etc/resolvconf/resolv.conf.d/head
-sudo systemctl restart resolvconf.service
-sudo resolvconf --enable-updates
-sudo resolvconf -u
+sudo systemctl restart resolvconf.service > /dev/null 
+sudo resolvconf --enable-updates > /dev/null 
+sudo resolvconf -u > /dev/null 
 
-echo "If you need to add another DNS IP"
-echo "Type in the Input Section"
-echo "OR type 'exit' if you don't"
+echo "\n\nIf you need to add another DNS IP"
+echo "\nType in the Input Section"
+echo "\nOR type 'exit' if you don't"
 read -p "Input: " ans
 if [ $ans = "exit" ]; then
     exit
 else 
     sudo echo "nameserver $ans" >> /etc/resolvconf/resolv.conf.d/head
-    sudo systemctl restart resolvconf.service
-    sudo resolvconf --enable-updates
-    sudo resolvconf -u
+    sudo systemctl restart resolvconf.service > /dev/null 
+    sudo resolvconf --enable-updates > /dev/null 
+    sudo resolvconf -u > /dev/null 
 
 fi
