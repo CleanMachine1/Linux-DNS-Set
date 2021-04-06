f4c911a5d10fccbfd6b668c7aa6b0769600d773e
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-06 22:07:56 +0000

    New lines

--- main.sh
+++ main.sh
@@ -2,16 +2,19 @@
 sudo apt install cowsay -y
 /usr/games/cowsay -f tux "This script was made by CleanMachine1"
 sleep 3
-echo -e "\n \n \n"
+echo " "
+echo " "
 sudo apt update > /dev/null && sudo apt install resolvconf -y > /dev/null
 sudo systemctl enable resolvconf.service > /dev/null && sudo systemctl start resolvconf.service > /dev/null
-echo -e "\n \n \n"
+echo " "
+echo " "
 read -p "Enter DNS IP Primary: " ip1
 echo "nameserver $ip1" > /etc/resolvconf/resolv.conf.d/head
 sudo systemctl restart resolvconf.service > /dev/null 
 sudo resolvconf --enable-updates > /dev/null 
 sudo resolvconf -u > /dev/null 
-echo -e "\n \n \n"
+echo ""
+echo ""
 echo "If you need to add another DNS IP"
 echo "Type in the Input Section "
 echo "OR type 'exit' if you don't"
