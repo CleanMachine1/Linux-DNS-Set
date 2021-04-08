f2487dc97d1c8ac889f30f4ae8964b91a827abae
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-08 21:47:56 +0100

    Added Clears and cowsays to keep the tux at the top

--- DNSSet.sh
+++ DNSSet.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 sudo apt install cowsay -y
+clear
 /usr/games/cowsay -f tux "This script was made by CleanMachine1"
 sleep 1
 echo " "
 echo " "
 sudo apt update > /dev/null && sudo apt install resolvconf -y > /dev/null
+clear
 sudo systemctl enable resolvconf.service > /dev/null && sudo systemctl start resolvconf.service > /dev/null
+clear
+/usr/games/cowsay -f tux "This script was made by CleanMachine1"
+echo "Step 2"
 echo " "
 echo " "
 read -p "Enter DNS IP Primary: " ip1
 echo "nameserver $ip1" > /etc/resolvconf/resolv.conf.d/head
+clear
 sudo systemctl restart resolvconf.service > /dev/null 
+clear
 sudo resolvconf --enable-updates > /dev/null 
+clear
 sudo resolvconf -u > /dev/null 
+clear
+/usr/games/cowsay -f tux "This script was made by CleanMachine1"
+echo "Step 3"
 echo ""
 echo ""
 echo "If you need to add another DNS IP"
@@ -23,8 +34,11 @@ if [ $ans = "exit" ]; then
     exit
 else 
     sudo echo "nameserver $ans" >> /etc/resolvconf/resolv.conf.d/head
+    clear
     sudo systemctl restart resolvconf.service > /dev/null 
+    clear
     sudo resolvconf --enable-updates > /dev/null 
+    clear
     sudo resolvconf -u > /dev/null 
-
+    clear
 fi
